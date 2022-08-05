5c6905c1f233167e1251ffe1ad484e77dcda1417
Olaleye Hairat <SammieKei40>
2022-08-05 10:10:27 +0100

    Updated Portfolio

--- src/Components/portfolio/portfolio.js
+++ src/Components/portfolio/portfolio.js
@@ -4,6 +4,7 @@ import shoppingimg from '../../assets/shopping.png'
 import reactapp from '../../assets/React App - Google Chrome 7_24_2022 11_04_03 AM.png'
 import foodapp from '../../assets/food.png'
 import social from '../../assets/social.png'
+import newsapp from '../../assets/news app.png'
 
 export default function Portfolio (){
     return(
@@ -12,6 +13,14 @@ export default function Portfolio (){
             <h2>Portfolio</h2>
             
             <div className='container portfolio__container'>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={newsapp} alt=""/>
+                    </div>
+                        <h3>Real Time News App with React JS</h3>
+                        <a href="#" className="btn" target='__blank'>Not in Production</a>
+                </article>
+
                 <article className="portfolio__item">
                     <div className="portfolio__item-image">
                         <img src={shoppingimg} alt=""/>
